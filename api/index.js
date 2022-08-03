2a5bc20cb85e99d0bef3830989fa0482583f692c
Luca Del Puppo <luca@delpuppo.net>
2022-08-03 20:13:10 +0200

    test api

--- api/index.js
+++ api/index.js
@@ -5,23 +5,23 @@ const chalk = require('chalk');
 
 console.log('Starting server...');
 
-// function prettyPrint(object) {
-//   console.log();
-//   console.log(chalk.bold('Resources'));
+function prettyPrint(object) {
+  console.log();
+  console.log(chalk.bold('Resources'));
 
-//   for (const prop in object) {
-//     console.log(`  /${prop}`);
-//   }
-//   console.log();
-// }
+  for (const prop in object) {
+    console.log(`  /${prop}`);
+  }
+  console.log();
+}
 
 const server = jsonServer.create({
   noCors: true,
 })
-// const router = jsonServer.router(path.join(process.cwd(), 'server', 'db.json'))
+const router = jsonServer.router(path.join(process.cwd(), 'server', 'db.json'))
 const middlewares = jsonServer.defaults()
 
-// server.db = router.db
+server.db = router.db
 
 server.use(middlewares)
 // server.use(auth)
@@ -33,15 +33,17 @@ server.get('/', (request, response) => {
 // server.use(router)
 
 // if (require.main === module) {
-//   server.listen(5000, () => {
-//     prettyPrint(router.db.getState());
-//     console.log('JSON Server is running')
-//   })
+server.listen(5000, () => {
+  prettyPrint(router.db.getState());
+  console.log('JSON Server is running')
+})
 // }
 
 // export default server;
 
+
 export default function handler(request, response) {
+  console.log('Starting request...');
   const { name } = request.query;
   response.status(200).send(`Hello ${name}!`);
 }
