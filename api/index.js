34ad6d52ad9ef5c0df4057f3e3f74878e5069fcc
Luca Del Puppo <luca@delpuppo.net>
2022-08-03 19:39:52 +0200

    test api

--- api/index.js
+++ api/index.js
@@ -34,19 +34,7 @@
 
 // module.exports = server;
 
-const app = require('express')();
-const { v4 } = require('uuid');
-
-app.get('/', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-module.exports = app;
+export default function handler(request, response) {
+  const { name } = request.query;
+  response.status(200).send(`Hello ${name}!`);
+}
