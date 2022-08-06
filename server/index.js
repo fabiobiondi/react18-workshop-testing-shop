6f17de7d5f324882e98b3473c04373f2629ea5c3
Luca Del Puppo <luca@delpuppo.net>
2022-08-06 12:13:39 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
+const cors = require('cors');
 
 const app = jsonServer.create({
   noCors: true
@@ -10,6 +11,7 @@ const router = jsonServer.router('db.json')
 app.db = router.db
 
 // You must apply the auth middleware before the router
+app.use(cors());
 app.use(auth)
 app.use(router)
 app.listen(process.env.PORT || 3001)
