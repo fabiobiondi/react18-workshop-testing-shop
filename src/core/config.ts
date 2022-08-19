08639b9d3c487cb6072751a34ee2fccf811b7eff
Luca Del Puppo <luca@delpuppo.net>
2022-08-19 22:36:34 +0200

    add new unit test

--- src/core/config.ts
+++ src/core/config.ts
@@ -2,13 +2,12 @@ type AppConfig = {
   baseApiUrl: string;
 };
 
-function createAppConfig(): AppConfig {
-  console.log("createAppConfig", process.env.REACT_APP_API_URL);
-  if (!process.env.REACT_APP_API_URL)
+export function createAppConfig(env = process.env): AppConfig {
+  if (!env.REACT_APP_API_URL)
     throw new Error("REACT_APP_API_URL is not defined");
 
   return {
-    baseApiUrl: process.env.REACT_APP_API_URL,
+    baseApiUrl: env.REACT_APP_API_URL,
   };
 }
 
--- src/core/store/cart.store.test.ts
+++ src/core/store/cart.store.test.ts
@@ -50,4 +50,118 @@ describe("useCart", () => {
       },
     ]);
   });
+
+  it("should return the correct total cost of the items in the cart", () => {
+    const product2 = mockProducts[1];
+    const { result } = renderHook(() => useCart());
+
+    act(() => result.current.addToCart(product, color, size));
+    act(() => result.current.addToCart(product, color, size));
+
+    act(() => result.current.addToCart(product2, color, size));
+
+    const totalCost = result.current.totalCost();
+
+    expect(totalCost).toBe(product.price * 2 + product2.price);
+  });
+
+  it("should return the correct amount of item in the cart", () => {
+    const product2 = mockProducts[1];
+    const { result } = renderHook(() => useCart());
+
+    act(() => result.current.addToCart(product, color, size));
+    act(() => result.current.addToCart(product, color, size));
+    act(() => result.current.addToCart(product, color, size));
+
+    act(() => result.current.addToCart(product2, color, size));
+    act(() => result.current.addToCart(product2, color, size));
+    act(() => result.current.addToCart(product2, color, size));
+
+    const totalItems = result.current.totalItems();
+
+    expect(totalItems).toBe(6);
+  });
+
+  it("should remove the item from the cart", () => {
+    const { result } = renderHook(() => useCart());
+    act(() => result.current.addToCart(product, color, size));
+    act(() => result.current.addToCart(product, color, size));
+    act(() => result.current.addToCart(product, color, size));
+    const currentItem = result.current.items[0];
+
+    expect(result.current.items.length).toBe(1);
+
+    act(() => result.current.removeFromCart(currentItem));
+
+    expect(result.current.items.length).toBe(0);
+  });
+
+  it('should clean the cart when the action "cleanCart" is called', () => {
+    const product2 = mockProducts[1];
+    const { result } = renderHook(() => useCart());
+    act(() => result.current.addToCart(product, color, size));
+    act(() => result.current.addToCart(product, color, size));
+    act(() => result.current.addToCart(product2, color, size));
+
+    expect(result.current.items.length).toBe(2);
+
+    act(() => result.current.cleanCart());
+
+    expect(result.current.items.length).toBe(0);
+    expect(result.current.totalCost()).toBe(0);
+  });
+
+  it("should increment the quantity of an item already in the cart", () => {
+    const { result } = renderHook(() => useCart());
+    act(() => result.current.addToCart(product, color, size));
+    const currentItem = result.current.items[0];
+
+    expect(result.current.items[0].qty).toBe(1);
+    act(() => result.current.incQty(currentItem));
+
+    expect(result.current.items[0].qty).toBe(2);
+    expect(result.current.items.length).toBe(1);
+  });
+
+  describe("decQty", () => {
+    it("should decrement the quantity of an item already in the cart", () => {
+      const { result } = renderHook(() => useCart());
+      act(() => result.current.addToCart(product, color, size));
+      const currentItem = result.current.items[0];
+
+      act(() => result.current.incQty(currentItem));
+      act(() => result.current.incQty(currentItem));
+      expect(result.current.items[0].qty).toBe(3);
+
+      act(() => result.current.decQty(currentItem));
+
+      expect(result.current.items[0].qty).toBe(2);
+      expect(result.current.items.length).toBe(1);
+    });
+
+    it("should not decrement the quantity of an item if the quantity is 1", () => {
+      const { result } = renderHook(() => useCart());
+      act(() => result.current.addToCart(product, color, size));
+      const currentItem = result.current.items[0];
+
+      act(() => result.current.decQty(currentItem));
+
+      expect(result.current.items[0].qty).toBe(1);
+    });
+
+    it("should decrement only the item that have the same key", () => {
+      const product2 = mockProducts[1];
+      const { result } = renderHook(() => useCart());
+      act(() => result.current.addToCart(product, color, size));
+      act(() => result.current.addToCart(product, color, size));
+      act(() => result.current.addToCart(product2, color, size));
+      act(() => result.current.addToCart(product2, color, size));
+      const currentItem = result.current.items[0];
+
+      act(() => result.current.decQty(currentItem));
+
+      expect(result.current.items[0].qty).toBe(1);
+      expect(result.current.items[1].qty).toBe(2);
+    });
+  });
 });
