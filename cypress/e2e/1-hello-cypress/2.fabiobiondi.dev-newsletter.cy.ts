f8c638cf8e208286443bd2a37ae503d65a6243f5
Fabio Biondi <hello@fabiobiondi.io>
2022-08-27 13:21:43 +0200

    clean hello-cypress text

--- cypress/e2e/1-hello-cypress/2.fabiobiondi.dev-newsletter.cy.ts
+++ cypress/e2e/1-hello-cypress/2.fabiobiondi.dev-newsletter.cy.ts
@@ -1,40 +1,41 @@
-
-describe('Google Search', () => {
+describe("Google Search", () => {
   beforeEach(() => {
-    // stub image
-    cy.intercept({
-      method: 'GET',
-      url: /\/.+\.(png|svg|jpeg|webp|jpg|webp)/            // add the regex directly
-    }, { fixture: "image2_placeholder.png" })
+    // stub images
+    // NOTE: you may need to disable cache in Browser DevTools
+    cy.intercept(
+      {
+        method: "GET",
+        url: /\/.+\.(png|svg|jpeg|webp|jpg)/, // add the regex directly
+      },
+      { fixture: "image2_placeholder.png" }
+    );
 
+    cy.visit("https://www.fabiobiondi.dev");
+  });
 
-    cy.visit('https://www.fabiobiondi.dev')
+  it("should subscribe the newsletter", () => {
+    cy.get('input[placeholder="Enter your email"').type(
+      "fakeuser@doesnotexist.it"
+    );
+    cy.get("button").contains("Subscribe").should("be.disabled");
+  });
 
-    // cy.intercept("https://www.fabiobiondi.dev/_next/image?url=**", { fixture: "image2_placeholder.png"})
-  })
+  it.only("should subscribe the newsletter", () => {
+    cy.get('input[placeholder="Enter your email"').type(
+      "fakeuser@doesnotexist.it"
+    );
 
-  it('should subscribe the newsletter', () => {
-    cy.get('input[placeholder="Enter your email"').type('fakeuser@doesnotexist.it');
-    cy.get('button').contains('Subscribe').should('be.disabled')
-  })
-
-  it.only('should subscribe the newsletter', () => {
-    cy.get('input[placeholder="Enter your email"').type('fakeuser@doesnotexist.it');
-
-    // ❌ 1. flag privacy policy checkbox
+    // ❌ Solution 1. flag privacy policy checkbox
     // NOTE: it only works if there is only one checkbox
     // cy.get('[type="checkbox"]').check()
 
-    // ✅ 2. flag privacy policy checkbox
-    cy
-      .get('label')                           // get a label
-      .contains('I have read privacy')        // with the privacy policy
-      .siblings('[type="checkbox"]')          // and get its sibling checkbox
-      .check()                                // checkbox
+    // ✅ Solution 2. flag privacy policy checkbox
+    // NOTE: select the adiacent label and get the sibling checkbox
+    cy.get("label") // get a label
+      .contains("I have read privacy") // with the privacy policy
+      .siblings('[type="checkbox"]') // and get its sibling checkbox
+      .check(); // checkbox
 
-    cy.get('button').contains('Subscribe').should('be.enabled')
-  })
-
-
-
-})
+    cy.get("button").contains("Subscribe").should("be.enabled");
+  });
+});
